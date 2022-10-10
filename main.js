b1694a73a8e58857e4ee7e7bf434597c1046799c
Alexander L. Wolf <alucaswolf@gmail.com>
2022-10-11 00:09:24 +0200

    Updated Version 1

--- main.js
+++ main.js
@@ -245,3 +245,22 @@ function random(){
   currentReview = randomNumber;
   updateReview();
   }
+
+// Review
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
